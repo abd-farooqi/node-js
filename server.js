d01466c09365660fe2576cd004d45b3fd6e658ad
abd-farooqi <abdullahfarooqi332008@gmail.com>
2026-02-10 23:38:47 +0500

    init

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 import http from "http";
 
-const PORT = 8000;
+const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-  //   res.write("Hello World!");
   //   res.setHeader("Content-Type", "text/html");
   //   res.statysCode = 404;
+  console.log(req.url);
+  console.log(req.method);
 
-  res.writeHead(500, { "Content-Type": "text/html" });
-  res.end(JSON.stringify({ message: "Server Error!" }));
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end("<h1>Hello World!</h1>");
 });
 
 server.listen(PORT, () => {
