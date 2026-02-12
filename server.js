1b257800a98592b8167831f9557970ab59c5d1f8
abd-farooqi <abdullahfarooqi332008@gmail.com>
2026-02-13 00:08:03 +0500

    updated

--- server.js
+++ server.js
@@ -3,13 +3,25 @@ import http from "http";
 const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-  //   res.setHeader("Content-Type", "text/html");
-  //   res.statysCode = 404;
-  console.log(req.url);
-  console.log(req.method);
-
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("<h1>Hello World!</h1>");
+  try {
+    if (req.method === "GET") {
+      if (req.url === "/") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>Homepage</h1>");
+      } else if (req.url === "/about") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>About</h1>");
+      } else {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("<h1>Page Not Found</h1>");
+      }
+    } else {
+      throw new Error("Method not allowed");
+    }
+  } catch (error) {
+    res.writeHead(500, { "Content-Type": "plain" });
+    res.end("Server Error");
+  }
 });
 
 server.listen(PORT, () => {
