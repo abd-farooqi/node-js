f929d8bfa75577a5a998ca0c117a74ff156db261
abd-farooqi <abdullahfarooqi332008@gmail.com>
2026-02-19 07:40:34 +0500

    feat: update files

--- cryptoDemo.js
+++ cryptoDemo.js
@@ -1,2 +1,12 @@
+import crypto from "crypto";
+
 // createHash()
-const hash = crypto.hash("sha256");
+const hash = crypto.createHash("sha256");
+hash.update("password1234");
+console.log(hash.digest("hex"));
+
+// randomBytes()
+crypto.randomBytes(16, (err, buff) => {
+  if (err) throw err;
+  console.log(buff.toString("hex"));
+});
